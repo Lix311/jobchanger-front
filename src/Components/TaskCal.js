bd36b65fe36d1a313e94d721c1126619f87cd5e4
Lix311 <alexsterrantino@gmail.com>
2020-10-22 19:39:51 -0400

    cal added needs css and function

---
+++ src/Components/TaskCal.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import {Calendar, momentLocalizer} from 'react-big-calendar'
+import moment from 'moment'
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment)
+
+let dates = () => {
+    return {
+        title:'alex interview',
+        start: new Date('October 17, 2020 03:24:00'),
+        end: new Date ('October 18, 2020 03:24:00')
+    }
+}
+
+class TaskCal extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div style={{height: '100vh', margin: '10px'}}>
+            <Calendar
+            localizer={localizer}
+            events={
+                [{
+                    title:'alex interview',
+                    start: new Date('October 17, 2020 03:24:00'),
+                    end: new Date ('October 18, 2020 03:24:00')
+                }]}
+            startAccessor="start"
+            endAccessor="end"
+         />
+         </div>
+        );
+    }
+}
+ 
+export default TaskCal;
--- src/Containers/TaskContainer.js
+++ src/Containers/TaskContainer.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Tasks from '../Components/Tasks'
 import Badges from '../Components/Badges'
 import TaskJumbo from '../Components/TaskJumbo'
+import TaskCal from '../Components/TaskCal'
 
 const TaskContainer = () => {
     return (  
         <div>
            <Badges/>
-           <div className='taskjumbo'>
-           <TaskJumbo/>
-           </div>
+          
+            <TaskCal/>
+          {/* <div className='taskjumbo'>
+            <TaskJumbo/>
+           </div> */}
           
         </div>
     );
