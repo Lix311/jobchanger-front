40a1d9dde143e2b0a8e072f91732bdd36f7a7bf5
Lix311 <alexsterrantino@gmail.com>
2021-01-04 17:06:09 -0500

    carousel done

--- src/Components/JobDetails.js
+++ src/Components/JobDetails.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import {Modal, Form, Button, Card, Row} from 'react-bootstrap'
+import {Modal, Form, Button, Card, Row, Carousel} from 'react-bootstrap'
 import '../App.css'
+
 import {Markup} from 'interweave'
 
 // make this have state to hold values?
@@ -20,7 +21,60 @@ class JobDetails extends Component {
            
            {/* <Modal show={this.props.show} onHide={this.props.onHide} animation={false}> */}
            
-        { this.props.show === false ? <h1>Sliding Modal</h1> : <Modal.Dialog>
+        { this.props.show === false ? <Carousel>
+  
+  <Carousel.Item>
+    
+    <img
+      className="car-image"
+      src="https://images.pexels.com/photos/5408915/pexels-photo-5408915.jpeg?cs=srgb&dl=pexels-olya-kobruseva-5408915.jpg&fm=jpg"
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <div class="car-text">
+        <h3>Schedule</h3>
+        <p>Take a look at the JobChanger Calender to keep track of your next interview. </p>
+      </div>
+    </Carousel.Caption>
+  </Carousel.Item>
+  
+
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://images.pexels.com/photos/4491469/pexels-photo-4491469.jpeg?cs=srgb&dl=pexels-karolina-grabowska-4491469.jpg&fm=jpg"
+
+      alt="Second slide"
+    />
+<Carousel.Caption>
+      <div class="car-text">
+        <h3>Organize</h3>
+        <p>Keep track of your job applications using the JobChanger Planner. Keep up-to-date on times, dates, and other valuble information.  </p>
+      </div>
+
+    </Carousel.Caption>
+  </Carousel.Item>
+  
+  
+  
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src="https://images.pexels.com/photos/5424647/pexels-photo-5424647.jpeg?cs=srgb&dl=pexels-amina-filkins-5424647.jpg&fm=jpg"
+
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <div class="car-text">
+        <h3>Make a Change</h3>
+        <p>Start using JobChanger today and land the job you've always dreamed of.</p>
+      </div>
+
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel> : <Modal.Dialog>
   <Modal.Header>
   {console.log(this.props.show)}
   {this.props.currentDetails.logo ? <img className='movie-img' src={this.props.currentDetails.logo}/> : ''}
