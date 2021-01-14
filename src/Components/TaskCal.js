1af7e54a7d9c318e7e272c3199a27869c5366460
Lix311 <alexsterrantino@gmail.com>
2021-01-13 22:41:19 -0500

    big bug fixed calendar

--- src/Components/TaskCal.js
+++ src/Components/TaskCal.js
@@ -6,11 +6,22 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 const localizer = momentLocalizer(moment)
 class TaskCal extends Component {
     state = {  
-        events: []
+        events: [],
+        deletedevents: [],
+        addedevents:[]
     }
 
     
-
+    checkDates = () => {
+        let datecount = 0;
+        this.props.currentJobs.map(job =>  {
+            if (job.job_date != null) {
+                datecount++
+            }
+        })
+        return datecount
+    }
+    
     setDates = () => {
 
         this.props.currentJobs.map(job => {
@@ -22,22 +33,26 @@ class TaskCal extends Component {
                 console.log('null')
                 job.job_time.length < 7 ? newtime = job.job_date + 'T' + job.job_time + ':00.000' : newtime = job.job_date + job.job_time.replace('2000-01-01','').replace('Z','')
             
-
-            this.state.events.push({
+            let newevent = {
                 start: new Date(newtime), // newtime is '2020-10-17T21:45' 
                 end: new Date(newtime), 
                 title: `${job.company}`
-            })
-        }
+            }
+
+            
+                this.state.events.push(newevent);
+
+    }
             const newDateWithTimezonedString = new Date("2020-10-28T21:32:00.000Z");  // Wed Oct 28 2020 17:32:00 GMT-0400 (Eastern Daylight Time)
             const newDateNoTimezonedString = new Date("2020-10-28T21:32:00.000");  // Wed Oct 28 2020 21:32:00 GMT-0400 (Eastern Daylight Time)
         })
     }
 
     render() { 
-        
+        this.state.events = [];
         this.setDates();
         return (  
+            
         <div className='wierdcal'>
             <Calendar
                 localizer={localizer}
